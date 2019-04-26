8ff03121dfef840dc27db00f1ee5835e3a688ea8
Eunjeong Park <ejisvip@gmail.com>
2019-04-26 11:13:30 +0900

    :construction: Create refactoring version of index.js

---
+++ index-refactor.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+// refactoring to promise ver.
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+const QUESTION = [
+	{
+		name: 'num',
+		type: 'input',
+		message: 'Enter your homework number :',
+		validate: val => {
+			val = val.trim()
+			if (isNaN(val) || val === '') {
+				return `Homework number only includes ${chalk.red.underline('numbers')}`;
+			} else {
+				return true;
+			}
+		}
+	}
+]
+
+inquirer.prompt(QUESTION)
+	.then(obj => {
+		const input = obj['num'].trim();
+		const DIR = __dirname + `/${input}/`
+		const HW_NAME = `${input}-homework.md`
+		const WS_NAME = `${input}-workshop.md`
+		const HW_CONTENT = `# ${input} Homework\n`
+		const WS_CONTENT = `# ${input} Workshop\n`
+
+		if (!fs.existsSync(DIR)) {
+			fs.mkdirSync(DIR)
+			// Create Homework file
+			fs.writeFile(HW_NAME, HW_CONTENT, err => {
+				if (err) throw err;
+				// Create Workshop fil
+				fs.writeFile(WS_NAME, WS_CONTENT, err => {
+					if (err) throw err;
+					// Move files into the directory
+					fs.rename(HW_NAME, DIR + HW_NAME, err => {
+						if (err) throw err;
+					})
+					fs.rename(WS_NAME, DIR + WS_NAME, err => {
+						if (err) throw err;
+					})
+					console.log(chalk.cyan('✔︎ Successfully created in ') + chalk.bgCyan.black(input) + chalk.cyan(' folder'));
+				});
+			});
+		} else {
+			console.log(chalk.yellow('» ') + chalk.bgYellow.black(input) + chalk.yellow(' folder already exists'));
+		}
+	});
